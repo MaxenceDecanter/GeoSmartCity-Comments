a0f73e263247f8bac89bc3094d2c9c7084a207e4
Zhao Hongyu <hongyu.zhao@edu.turkuamk.fi>
2017-01-09 17:04:48 +0200

    compeleted the responsive problems for commant page

--- comments/index.php
+++ comments/index.php
@@ -9,8 +9,8 @@
     <!-- Empty div for the map-->
 </div>
 
-<div class="col-sm-12" id="comment-page">
-    <div class="col-sm-12 comment-header">
+<div id="comment-page">
+    <div class="comment-header">
         <!--info-->
         <div id="information">
             <a href="http://geosmartcity.dc.turkuamk.fi/?page=home&lang=en">
@@ -22,32 +22,30 @@
         <!--comment-->
         <div id="routing">
             <!--Add routing page link here-->
-            <a href="#">
-                <span id="commentPic"><strong>R</strong></span>
+            <a href="http://geosmartcity.dc.turkuamk.fi/?page=home&lang=en">
+                <img src="images/infoicon.png" id="routingPic">
             </a>
         </div>
     </div>
     <div id="modeList">
-        <!--            <div><span>V</span></div>-->
-<!--        </li>-->
         <div id="event" data-toggle="tooltip" title="Event" data-toggle="tooltip" data-placement="top" href="javascript:void(0)" alt="/" onclick="selectIcon(event, 'mode-event','event');" onmouseover="tootips()"><div></div></div>
         <div id="info" data-toggle="tooltip" title="Information" data-toggle="tooltip" data-placement="top" href="javascript:void(0)" alt="/" onclick="selectIcon(event, 'mode-info','information');" onmouseover="tootips()"><div></div></div>
         <div id="other" data-toggle="tooltip" title="Other" data-toggle="tooltip" data-placement="top" href="javascript:void(0)" alt="/" onclick="selectIcon(event, 'mode-other','other')" onmouseover="tootips()"></div>
         <div id="problem" data-toggle="tooltip" title="Problem" data-toggle="tooltip" data-placement="top" href="javascript:void(0)" alt="/" onclick="selectIcon(event, 'mode-problem','problem')" onmouseover="tootips()"></div>
-<!--        <div id="viewAll" data-toggle="tooltip" title="Problem" data-toggle="tooltip" data-placement="top" href="javascript:void(0)" alt="/" onclick="selectIcon(event, 'mode-viewAll','/')" onmouseover="tootips()">-->
-        <div id="problem" data-toggle="tooltip" title="Problem" data-toggle="tooltip" data-placement="top" href="javascript:void(0)" alt="/" onclick="selectIcon(event, 'mode-problem','problem')" onmouseover="tootips()"></div>
+        <div id="problem" data-toggle="tooltip" title="View All" data-toggle="tooltip" data-placement="top" href="javascript:void(0)" alt="/" onclick="selectIcon(event, 'mode-problem','problem')" onmouseover="tootips()"></div>
     </div>
     <!--address bar-->
     <div>
         <input type="text" class="form-control" id="addr" placeholder="Address.." onKeyPress="if(event.keyCode == 13) addr_search();">
     </div>
-
     <!--Event mode-->
     <div id="mode-event" class="iconContent">
         <input type="text" class="form-control" id="title" placeholder="Title..">
         <textarea name="commentsForInfoMode" cols="30" rows="5" class="form-control" placeholder="Comments.."></textarea>
-        <button class="btn btn-default" style="width: 45%" onclick="multipleCloseEventDateFuncs()">Now</button>
-        <button class="btn btn-default" style="width: 45%; float: right" onclick="openEventDate()">Choose Date</button>
+        <div class="col-sm-12">
+            <button class="btn btn-default col-sm-6" onclick="multipleCloseEventDateFuncs()">Now</button>
+            <button class="btn btn-default col-sm-6" onclick="openEventDate()">Choose Date</button>
+        </div>
         <!--choose time tab-->
         <div class="col-sm-12 date-tab" id="event-date">
             <input type="date" class="form-control">
@@ -75,8 +73,10 @@
     <div id="mode-info" class="iconContent">
         <input type="text" class="form-control" name="title" placeholder="Title..">
         <textarea name="commentsForInfoMode" cols="30" rows="5" class="form-control" placeholder="Comments.."></textarea>
-        <button class="btn btn-default" style="width: 45%" onclick="multipleCloseInfoDateFuncs();">Now</button>
-        <button class="btn btn-default opentimeTab" style="width: 45%; float: right" onclick="openInfoDate();">Choose Date</button>
+        <div class="col-sm-12">
+            <button class="btn btn-default col-sm-6" onclick="multipleCloseEventDateFuncs()">Now</button>
+            <button class="btn btn-default col-sm-6" onclick="openEventDate()">Choose Date</button>
+        </div>
         <!--choose time tab-->
         <div class="col-sm-12 date-tab" id="info-date">
             <input type="date" class="form-control" name="date">
@@ -102,9 +102,7 @@
 
     <!--Other mode-->
     <div id="mode-other" class="iconContent">
-        <div class="col-sm-12">
-            <input type="text" class="form-control" placeholder="Type title here..">
-        </div>
+        <input type="text" class="form-control" id="title" placeholder="Title..">
         <textarea name="commentsForInfoMode" cols="30" rows="5" class="form-control" placeholder="Comments.."></textarea>
         <!--Save & previous & cancel button set-->
         <button class="btn btn-default" style="width: 45%" onclick="validateComment(true);">Save</button>
@@ -115,8 +113,10 @@
     <div id="mode-problem" class="iconContent">
         <input type="text" class="form-control" id="title" placeholder="Title..">
         <textarea name="commentsForInfoMode" cols="30" rows="5" class="form-control" placeholder="Comments.."></textarea>
-        <button class="btn btn-default" style="width: 45%" onclick="multipleCloseProblemDateFuncs();">Now</button>
-        <button class="btn btn-default" style="width: 45%; float: right" onclick="openProblemDate();">Choose Date</button>
+        <div class="col-sm-12">
+            <button class="btn btn-default col-sm-6" onclick="multipleCloseEventDateFuncs()">Now</button>
+            <button class="btn btn-default col-sm-6" onclick="openEventDate()">Choose Date</button>
+        </div>
         <!--choose date tab-->
         <div class="col-sm-12 date-tab" id="problem-date">
             <input type="date" class="form-control">
